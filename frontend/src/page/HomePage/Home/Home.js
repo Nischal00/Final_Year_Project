65354a3675c1e18b1fab15c53ef00f53bb21ee95
Nischal00 <nischalbhandari22@gmail.com>
2024-04-08 23:08:59 +0545

    realtime database fixed

--- frontend/src/page/HomePage/Home/Home.js
+++ frontend/src/page/HomePage/Home/Home.js
@@ -113,7 +113,7 @@ class HomePage extends Component {
       permissionDenied: false,
       textvalue: "",
     };
-    this.HOMEURL="http://localhost:8080";
+    this.HOMEURL = "http://localhost:3000";
     // this.HOMEURL = "https://entercoding-gces.herokuapp.com";
     // this.ENDPOINT = "https://entercoding-api-gces.herokuapp.com";
     this.ENDPOINT = "http://localhost:8080";
@@ -300,8 +300,16 @@ class HomePage extends Component {
           this.handleThemeChange(JSON.parse(storageTheme));
         }
         const config = {
-          apiKey: "AIzaSyA5nYeooM0GO6hgan73XCsvfZPykfHjfXs",
-          databaseURL: "https://fire-editor-f16fa.firebaseio.com",
+          // apiKey: "AIzaSyA5nYeooM0GO6hgan73XCsvfZPykfHjfXs",
+          apiKey: "AIzaSyDKK-uAr09X2I3QfJjt_Ft0nzx-CKgvOKM",
+          databaseURL: "https://final-year-project-3553d-default-rtdb.firebaseio.com/",
+          // apiKey: "AIzaSyDKK-uAr09X2I3QfJjt_Ft0nzx-CKgvOKM",
+          // authDomain: "final-year-project-3553d.firebaseapp.com",
+          // projectId: "final-year-project-3553d",
+          // storageBucket: "final-year-project-3553d.appspot.com",
+          // messagingSenderId: "1045507360080",
+          // appId: "1:1045507360080:web:a5a4700367ae80cc29d855",
+          // measurementId: "G-64GNZ48RE7",
         };
         window.firebase.initializeApp(config);
 
